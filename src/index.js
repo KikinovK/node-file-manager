70db03670f1d54edb123270e76e229f093a600f5
Kostaintyn Kikinov <kikinov.k@gmail.com>
2024-02-03 00:24:25 +0200

    feat: add application exits

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
+import readline from 'readline';
+import './utils/capitalize.js';
+import { parseUserName } from './services/parseArgs.js';
+
 const args = process.argv.slice(2);
+const userName = parseUserName(args);
 
-const userName = args.filter(
-  (arg) => arg.startsWith('--') &&
-    arg.slice(2).startsWith('username')
-)[0].split('=')[1];
+if (!userName) {
+  console.error('Username not provided or invalid');
+  process.exit(1);
+}
 
-console.log(`Welcome to the File Manager, ${userName[0].toUpperCase()}${userName.slice(1)}`);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question(`Welcome to the File Manager, ${userName} \n`, (command) => {
+  if (command === 'exit') rl.close();
+});
+
+
+process.on('exit', () => {
+  console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+});
